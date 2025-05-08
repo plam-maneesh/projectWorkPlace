626f7614a02d536dd60f919a54ab2ccee3382c5c
maneesh <plammaneesh@gmail.com>
2025-05-08 22:10:18 +0530

    crating signup form and display list fo array in ul list learn renderUserList, forEach,and appendchild

--- index.js
+++ index.js
@@ -1,59 +1,36 @@
 class signupForm {
-
-
   constructor() {
+      this.Users = [];
+      this.form = document.getElementById('signupForm');
+      this.name = document.getElementById('name');
+      this.password = document.getElementById('password');
+      this.submit = document.getElementById('submit');
 
-    this.Users = [];
-    this.form = document.getElementById('signupForm');
-    this.name = document.getElementById('name');
-    this.password = document.getElementById('password');
-    this.submit = document.getElementById('submit');
-    const userList = document.getElementById('userList');
+      this.submit.addEventListener('click', (event) => {
+          event.preventDefault();
+          let nameValue = this.name.value;
+          let passwordValue = this.password.value;
 
-    this.name.addEventListener('input', (event) => {
-
-      event.preventDefault(event);
-      let nameValue = event.target.value; // get the value of the name input
-
-
-    });
-
-
-    this.password.addEventListener('input', (event) => {
-
-      let passwordValue = event.target.value; // get the value of the password input
-
-    });
-  
-
-    this.submit.addEventListener('click', (event) => {
-      event.preventDefault(event);
-      let nameValue = this.name.value; // get the value of the name input
-      let passwordValue = this.password.value; // get the value of the password input
-
-      if (nameValue && passwordValue) {
-        this.Users.push({ name: nameValue, password: passwordValue })
-        this.name.value = '';
-        this.password.value = '';
-        this.renderUserList();
-        }
-      
-      }
-    );
-
+          if (nameValue && passwordValue) {
+              this.Users.push({ name: nameValue, password: passwordValue });
+              console.log("Users array after addition:", this.Users); // Debugging
+              this.name.value = '';
+              this.password.value = '';
+              this.renderUserList();
+          }
+      });
   }
 
-
-
-
   renderUserList() {
-    const userList = document.getElementById('userList');
-    userList.innerHTML = ''; // clear the list before rendering
-    this.Users.forEach((user) => {
-      const li = document.createElement('li');
-      li.textContent = `Name: ${user.name}, Password: ${user.password}`;
-      userList.appendChild(li);
-    });
+      const userList = document.getElementById('listUsers');
+      console.log("Users", this.Users); // Corrected to log this.Users
+      userList.innerHTML = ''; 
+      this.Users.forEach((user) => {
+          console.log("Rendering user:", user); // Debugging
+          const li = document.createElement('li');
+          li.textContent = `Name: ${user.name}, Password: ${user.password}`;
+          userList.appendChild(li);
+      });
   }
 }
 
